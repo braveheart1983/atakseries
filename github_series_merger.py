88a2418bf67bfe5dab19daac5be3ca7b7038bcc7
Braveheart is back <atakan__19833@hotmail.com>
2026-06-27 19:56:06 +0300

    Update github_series_merger.py

--- github_series_merger.py
+++ github_series_merger.py
@@ -21,7 +21,9 @@ CATEGORY_MAP = {
     "hbo": ("HBO Max", "hbo"),
     "netflix": ("Netflix", "netflix"),
     "puhutv": ("PuhuTV", "puhutv"),
+    "atv": ("ATV", "atv"),
     "star-tv": ("Star TV", "star-tv"),
+    "showtv": ("Show TV", "showtv"),
     "tabii": ("Tabii", "tabii"),
     "tod": ("TOD", "tod"),
     "yerli-diziler": ("Yerli Diziler", "yerli-diziler")
